f510ce8bb3898e50e3752b42feea8e2449f22465
Isaac de la Peña <isaacdlp@gmail.com>
2017-09-11 04:11:46 +0200

    Yahoo Download Fix Added

---
+++ BasicYahooFix.py
@@ -0,0 +1,27 @@
+# PyAlgoSamples
+# Examples using the PyAlgoTrade Library
+#
+# Copyright 2015-2017 Isaac de la Pena
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+.. moduleauthor:: Isaac de la Pena <isaacdlp@agoraeafi.com>
+"""
+
+from pandas_datareader import data as pdr
+from fix_yahoo_finance import pdr_override
+
+pdr_override()
+data = pdr.get_data_yahoo("AMS.MC", start="2015-01-01", end="2015-12-31")
+data.to_csv("AMS.MC-2015.csv")
